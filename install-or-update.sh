9dad6c4ffb214842110a8f1c07e82ebd25dea878
gabriele.lana <gabriele.lana@cleancode.it>
2014-01-09 00:19:27 +0100

    copy files instead of symlink, planning to have themes so copy files would be necessary

--- install-or-update.sh
+++ install-or-update.sh
@@ -5,6 +5,18 @@ cd "$(dirname "$0")"
 echo "update dotfiles..."
 git pull
 
+echo "install configuration files..."
+rm -rf ~/.i3 && cp -rf $PWD/i3 ~/.i3
+rm -rf ~/.bin && cp -rf $PWD/bin ~/.bin
+cp -f $PWD/ackrc ~/.ackrc
+cp -f $PWD/gitconfig ~/.gitconfig
+cp -f $PWD/dunstrc ~/.dunstrc
+cp -f $PWD/gtkrc-2.0 ~/.gtkrc-2.0
+cp -f $PWD/xinitrc ~/.xinitrc
+cp -f $PWD/xinitrc ~/.xsession
+cp -f $PWD/xinitrc ~/.xprofile
+cp -f $PWD/xmodmap ~/.xmodmap
+
 echo "update dependencies..."
 mkdir -p $PWD/.dependencies
 for project in dotfiles-secrets awesome-terminal-fonts; do
@@ -29,49 +41,24 @@ fc-cache -fv ~/.fonts
 
 echo "setup ssh..."
 mkdir -p ~/.ssh
-cp -f $PWD/.dependencies/dotfiles-secrets/ssh ~/.ssh
+cp -f $PWD/.dependencies/dotfiles-secrets/ssh/* ~/.ssh
 cp -f $PWD/.dependencies/dotfiles-secrets/netrc ~/.netrc
 
-
-
-
-
-ln -sfT $PWD/ackrc ~/.ackrc
-ln -sfT $PWD/gitconfig ~/.gitconfig
-
-[[ ! -h ~/.bin ]] && rm -rf ~/.bin
-ln -sfT $PWD/bin ~/.bin
-
-[[ ! -h ~/.i3 ]] && rm -rf ~/.i3
-ln -sfT $PWD/i3 ~/.i3
-
-ln -sfT $PWD/dunstrc ~/.dunstrc
-ln -sfT $PWD/gtkrc-2.0 ~/.gtkrc-2.0
-
-ln -sfT $PWD/xinitrc ~/.xinitrc
-ln -sfT $PWD/xinitrc ~/.xsession
-ln -sfT $PWD/xinitrc ~/.xprofile
-ln -sfT $PWD/xdefaults ~/.xdefaults
-ln -sfT $PWD/xmodmap ~/.xmodmap
-
-[[ ! -h ~/.vim ]] && rm -rf ~/.vim
-ln -sfT $PWD/vim ~/.vim
-ln -sfT $PWD/vimrc ~/.vimrc
+echo "setup vim..."
+rm -rf ~/.vim && cp -rf $PWD/vim ~/.vim
+cp -f $PWD/vimrc ~/.vimrc
 if [ ! -d ~/.vim/bundle/vundle ]; then
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
 vim +BundleInstall! +qall > /dev/null 2>&1
 
+echo "setup zsh..."
 if [ ! -d ~/.oh-my-zsh ]; then
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
-
-for file in `ls $PWD/zsh`; do
-  ln -sfT $PWD/zsh/$file ~/.oh-my-zsh/custom/$file
-done
-
-ln -sfT $PWD/zshrc ~/.zshrc
 export ZSH=~/.oh-my-zsh
+cp -rfu $PWD/zsh ~/.oh-my-zsh/custom
+cp -f $PWD/zshrc ~/.zshrc
 if which zsh > /dev/null; then
   zsh ~/.zshrc
   zsh $ZSH/tools/upgrade.sh
