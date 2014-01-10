de53c4e91f18847600ae65d97492675ee310db08
gabriele.lana <gabriele.lana@cleancode.it>
2014-01-10 13:46:51 +0100

    restore gnome-terminal configuration

--- install-or-update.sh
+++ install-or-update.sh
@@ -63,3 +63,7 @@ if which zsh > /dev/null; then
   zsh ~/.zshrc
   zsh $ZSH/tools/upgrade.sh
 fi
+
+echo "configure gnome terminal..."
+dconf reset -f "/org/gnome/terminal"
+cat $PWD/gnome-terminal.ini | dconf load "/org/gnome/terminal/"
