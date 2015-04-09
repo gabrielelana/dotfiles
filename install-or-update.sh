c7c4012178f72c035e2a77b8fc96d1420bb14267
gabriele.lana <gabriele.lana@cleancode.it>
2015-04-09 12:35:08 +0000

    Always show vim plugin installation progression

--- install-or-update.sh
+++ install-or-update.sh
@@ -76,11 +76,7 @@ cp -f $PWD/vimrc ~/.vimrc
 if [ ! -d ~/.vim/bundle/vundle ]; then
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
-if [ ${flags[without-x]} -eq 1 ]; then
-  vim +PluginInstall! +qall > /dev/null 2>&1
-else
-  vim +PluginInstall! +qall
-fi
+vim +PluginInstall! +qall
 
 echo "setup zsh..."
 if [ ! -d ~/.oh-my-zsh ]; then
