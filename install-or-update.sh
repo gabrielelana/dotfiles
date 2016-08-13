d38948cdf66928f76dc2ca064e0128cab92f381a
gabriele.lana <gabriele.lana@cleancode.it>
2016-08-13 11:13:20 +0200

    Re-enable vim configuration - update plugins (too long)

--- install-or-update.sh
+++ install-or-update.sh
@@ -66,12 +66,12 @@ if [ ${flags[without-identity]} -eq 0 ]; then
   fi
 fi
 
-# echo "setup vim..."
-# rm -rf ~/.vim && cp -rf $PWD/vim ~/.vim
-# cp -f $PWD/vimrc ~/.vimrc
-# if [ ! -d ~/.vim/bundle/vundle ]; then
-#   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-# fi
+echo "setup vim..."
+rm -rf ~/.vim && cp -rf $PWD/vim ~/.vim
+cp -f $PWD/vimrc ~/.vimrc
+if [ ! -d ~/.vim/bundle/vundle ]; then
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
 # vim +PluginInstall! +qall &>/dev/null
 
 echo "setup emacs..."
