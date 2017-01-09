28d7f4f7e5c697c3fd406046a8946beaec36fbdd
gabriele.lana <gabriele.lana@cleancode.it>
2017-01-09 09:01:31 +0100

    Emacs: closer to make pylint work in container

--- emacs.d/local-packages/conio-configuration.el
+++ emacs.d/local-packages/conio-configuration.el
@@ -24,14 +24,17 @@
          (output (shell-command-to-string command)))
     (message (replace-regexp-in-string "\x0D" "" output))))
 
-
+;; TODO pylint: the problem is the output file path which is relative instead of beign the file name, thinking about filtering the output in `~/.pylint` script...
+;; TODO pylint: try to define a pycheck checker with real source file avoiding to replace `flycheck_` in input path
 (defun conio-python-configuration ()
   "Conio python mode configuration"
-  ;; TODO: make it work... maybe defining a checker?
-  ;; (flycheck-mode +1)
+  (flycheck-mode +1)
+
+  ;; ;; INSIDE THE CONTAINER
   ;; (setq flycheck-python-pylint-executable (concat (projectile-project-root) "/.pylint"))
   ;; (setq flycheck-check-syntax-automatically '(mode-enabled save))
   ;; (setq-local flycheck-checkers '(python-pylint))
+  ;; (add-to-list 'flycheck-enabled-checkers 'python-pylint)
   ;; (setq-local flycheck-command-wrapper-function
   ;;             (lambda (command)
   ;;               (setf (nth (1- (length command)) command)
@@ -46,6 +49,14 @@
   ;;                      (nth (1- (length command)) command)))
   ;;                 (message "%s" command)
   ;;               command))
+
+  ;; ;; OUTSIDE THE CONTAINER
+  ;; (setq flycheck-python-pylint-executable "pylint3")
+  ;; (setq flycheck-check-syntax-automatically '(mode-enabled save))
+  ;; (add-to-list 'flycheck-enabled-checkers 'python-pylint)
+  ;; (setq-local flycheck-checkers '(python-pylint))
+
+  ;; ENABLE INDENT GUIDES
   (highlight-indent-guides-mode))
 
 (use-package highlight-indent-guides
