a239c9f067a32298723dd809f00fb70b44e985f5
gabriele.lana <gabriele.lana@cleancode.it>
2018-08-30 15:08:45 +0200

    Emacs: remove obsolete stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -800,15 +800,12 @@ options you can do it calling `(cc/shell-command-on-current-file
 (setq kmacro-ring-max 100)
 ;;; better performance sacrificing right-to-left languages
 (setq-default bidi-display-reordering nil)
-;;; fully redraw the display before processing input
-(setq redisplay-dont-pause t)
 ;;; alleviate bug of emacs-26 see https://github.com/emacs-helm/helm/issues/1976
 (when (eq emacs-major-version 26)
   (setq x-wait-for-event-timeout nil))
 
 ;;; shell-script-mode default configurations
 (setq sh-basic-offset 2)
-(setq sh-indentation 2)
 
 ;;; local configuration
 (cc/load-local-machine-configuration)
