f78d6475caf0388b64c60dd2d27b629243e89f43
gabriele.lana <gabriele.lana@gmail.com>
2019-12-15 17:40:39 +0100

    Emacs: TODO list to remember what needs to be fixed

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,5 @@
+;;; TODO: git-gutter is not enabled by default ???
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
