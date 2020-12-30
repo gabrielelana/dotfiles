c482a49d4cd2f42fb9a61b838e66c7ab6fb16583
gabriele.lana <gabriele.lana@gmail.com>
2020-12-30 11:24:21 +0100

    Update to Ubuntu 20.10

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# TODO!!!
+#   separate every section in functions and call the at the end
+#   so that it will be simpler to call/exclude them independently
+
 # TODO: install latest version of language && language server && utilities for: Elixir
-# TODO: install latest version of language && language server && utilities for: Rust
 # TODO: install latest version of language && language server && utilities for: JS/TS
+# TODO: install latest version of language && language server && utilities for: Rust
 # TODO: add to switch-theme a light theme that works
 # TODO: Firefox minimal theme?
 # TODO: Firefox theme customization with the same colors in switch-theme?
@@ -38,23 +42,24 @@ echo "Setup system..."
 #
 # Then you run ~/.dotfiles/bootstrap-profile.sh
 
-echo "Update package repositories..."
+# echo "Update package repositories..."
 sudo apt update
-
-echo "Install basic packages..."
+#
+# echo "Install basic packages..."
 sudo apt install -y \
+     apt-transport-https \
      aspell-it \
      autoconf \
      autocutsel \
      binutils \
      bison \
      build-essential \
+     ca-certificates \
      cmake \
      compton \
      coreutils \
      curl \
      dbus-x11 \
-     docker \
      dunst \
      entr \
      flex \
@@ -62,8 +67,10 @@ sudo apt install -y \
      g++-8 \
      gcc-multilib \
      git \
+     gnupg-agent \
      gnutls-bin \
      hsetroot \
+     htop \
      i3 \
      iitalian \
      jq \
@@ -93,7 +100,7 @@ sudo apt install -y \
      libtinfo5 \
      libtool \
      libwebkit2gtk-4.0-dev \
-     libwxgtk3.0-dev \
+     libwxgtk3.0-gtk3-dev \
      libxaw7-dev \
      libxft-dev \
      libxml2-dev \
@@ -104,6 +111,7 @@ sudo apt install -y \
      make \
      silversearcher-ag \
      slim \
+     software-properties-common \
      suckless-tools \
      texinfo \
      unclutter \
@@ -116,7 +124,8 @@ sudo apt install -y \
 
 echo "Cloning Emacs repository, be patient..."
 mkdir -p ~/tmp && cd ~/tmp || exit 1
-git clone git://git.sv.gnu.org/emacs.git emacs-build && cd emacs-build || exit 1
+[ ! -d emacs-build ] && git clone git://git.sv.gnu.org/emacs.git emacs-build
+cd emacs-build || exit 1
 EMACS_RELEASE=$(git describe --abbrev=0 --tags)
 echo "Compile and install Emacs $EMACS_RELEASE be patient..."
 git checkout "$EMACS_RELEASE"
@@ -129,32 +138,34 @@ git checkout "$EMACS_RELEASE"
               --without-dbus && \
   make -j"$(nproc)" && \
   make install
+
 # Cask
 curl -fsSkL https://raw.github.com/cask/cask/master/go | python
+
 # EVM
 sudo mkdir /usr/local/evm
 sudo chown "$USER": /usr/local/evm
 curl -fsSkL https://raw.github.com/rejeep/evm/master/go | bash
 
 echo "Install ShellCheck..."
-mkdir -p ~/bin
-if [ ! -f ~/bin/pup ]; then
+mkdir -p ~/.local/bin
+if [ ! -f ~/.local/bin/pup ]; then
   mkdir -p ~/tmp && cd ~/tmp || exit 1
   wget --quiet "https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz"
   tar --xz -xvf shellcheck-stable.linux.x86_64.tar.xz
-  cp shellcheck-stable/shellcheck ~/bin/shellcheck
+  cp shellcheck-stable/shellcheck ~/.local/bin/shellcheck
   rm -rf shellcheck-stable
   rm -f shellcheck-stable.linux.x86_64.tar.xz
 fi
 
 echo "Install pup..."
-mkdir -p ~/bin
-if [ ! -f ~/bin/pup ]; then
+mkdir -p ~/.local/bin
+if [ ! -f ~/.local/bin/pup ]; then
   cd ~/tmp || exit 1
   PUP_VERSION=0.4.0
   wget -q https://github.com/ericchiang/pup/releases/download/v${PUP_VERSION}/pup_v${PUP_VERSION}_linux_amd64.zip
   unzip pup_v${PUP_VERSION}_linux_amd64.zip
-  mv pup ~/bin
+  mv pup ~/.local/bin
   rm -rf pup_v${PUP_VERSION}_linux_amd64.zip
 fi
 
@@ -184,21 +195,11 @@ cd ~/.asdf && git checkout "$(git describe --abbrev=0 --tags)" && cd - || exit 1
 ~/.asdf/bin/asdf plugin-add kubectx
 ~/.asdf/bin/asdf plugin-add kustomize
 
+export PATH="~/.asdf/bin:$PATH"
+
 echo "Install ASDF direnv Plugin..."
 ~/.asdf/bin/asdf install direnv latest
 ~/.asdf/bin/asdf global direnv "$(~/.asdf/bin/asdf list direnv | tail -1 | tr -d ' ')"
-
-echo "Install Rust..."
-curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y
-
-echo "Install Haskell Stack..."
-curl -sSL https://get.haskellstack.org/ | sh -s - -f
-# To install haskell language server clone # https://github.com/haskell/haskell-language-server and follow README # instructions for stack projects
-# Then install utilities and libraries
-# $ stack install hlint
-# $ stack install QuickCheck
-# $ stack install HSpec
-# $ stack install ...
 
 echo "Install Go..."
 ~/.asdf/bin/asdf install golang latest
@@ -207,9 +208,6 @@ go get -u golang.org/x/tools/...
 go get -u golang.org/x/lint/golint
 go get -u github.com/rogpeppe/godef
 go get -u github.com/zmb3/gogetdoc
-
-echo "Install Starship..."
-curl -fsSL https://starship.rs/install.sh | bash
 
 echo "Install K8s Stuffs..."
 ~/.asdf/bin/asdf install kubectl latest
@@ -224,8 +222,23 @@ echo "Install NodeJS..."
 ~/.asdf/bin/asdf install nodejs latest
 ~/.asdf/bin/asdf global nodejs "$(~/.asdf/bin/asdf list nodejs | tail -1 | tr -d ' ')"
 npm install -g yarn prettier eslint typescript ts-node typescript-formatter dockerfile-language-server-nodejs tern tide http-server
-[[ (! -f /usr/bin/python) && (-f /usr/bin/python3) ]] sudo ln -s /usr/bin/python3 /usr/bin/python
+[[ (! -f /usr/bin/python) && (-f /usr/bin/python3) ]] && sudo ln -s /usr/bin/python3 /usr/bin/python
 npm install -g import-js --unsafe-per
+
+echo "Install Rust..."
+curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y
+
+echo "Install Haskell Stack..."
+curl -sSL https://get.haskellstack.org/ | sh -s - -f -d ~/.local/bin/
+# To install haskell language server clone # https://github.com/haskell/haskell-language-server and follow README # instructions for stack projects
+# Then install utilities and libraries
+# $ stack install hlint
+# $ stack install QuickCheck
+# $ stack install HSpec
+# $ stack install ...
+
+echo "Install Starship..."
+curl -fsSL https://starship.rs/install.sh | bash
 
 echo "Install Dhall..."
 mkdir -p ~/tmp/download-dhall && (cd ~/tmp/download-dhall || exit 1)
@@ -238,7 +251,11 @@ for f in $(curl --silent 'https://api.github.com/repos/dhall-lang/dhall-haskell/
 done
 cp ./bin/* ~/.local/bin && cd ~ && rm -rf ~/tmp/download-dhall
 
-echo "Configure Docker..."
+echo "Install and configure Docker..."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
