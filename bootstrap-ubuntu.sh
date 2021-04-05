1e253dec5713ba5b5b935e229c54a6c9928dca14
gabriele.lana <gabriele.lana@gmail.com>
2021-04-05 11:33:21 +0200

    Install docker-compose when bootstrap

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -264,6 +264,11 @@ sudo usermod -aG docker $USER
 newgrp docker
 sudo systemctl restart docker
 docker run hello-world || echo "! failed to install Docker"
+docker run hello-world && {
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+}
+
 # Shell testing harness
 echo "Install bats..." && (
   cd ~/tmp
